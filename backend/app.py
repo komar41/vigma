a9026551fd5b800e58fea365f1b6b8a77879e93e
Omar <komar3@uic.edu>
2024-05-30 10:42:55 -0500

    .gitignore is working

--- backend/app.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-data = {'fileLocation': '/path/to/your/directory'}
-response = requests.post('http://localhost:5000/send-data', json=data)
-print(response.status_code, response.json())
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
